a192ac1660165dbca31314f2a6a28bb674711df0
Samuel Williams <samuel.williams@shopify.com>
2026-05-24 22:54:20 +0900

    Select configured ALPN protocol

--- source/Protocol/QUIC/TLS/ServerContext.hpp
+++ source/Protocol/QUIC/TLS/ServerContext.hpp
@@ -25,7 +25,18 @@ namespace Protocol
 				void set_require_client_authentication(bool enabled);
 				
 			private:
-				
+				struct ClientHelloCallback
+				{
+					ptls_on_client_hello_t super;
+					ServerContext *context;
+				};
+
+				ClientHelloCallback _client_hello;
+
+				static int client_hello_callback(ptls_on_client_hello_t *self, ptls_t *ptls, ptls_on_client_hello_parameters_t *params);
+
+			protected:
+				virtual int client_hello(ptls_t *ptls, ptls_on_client_hello_parameters_t *params);
 			};
 		}
 	}
